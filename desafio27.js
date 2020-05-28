02104ed9406dff17a3fc4f74018270af4651c2b6
guicgs <etohlif@protonmail.com>
2020-05-28 18:42:05 -0300

    atualização 28

--- desafio27.js
+++ desafio27.js
@@ -1,3 +1,2 @@
 use dataFlights;
 db.voos.deleteMany({ $and: [{ "empresa.nome": "AZUL" }, { litrosCombustivel: { $lt: 400 } }] });
-// 538 voos deletados
--- desafio28.js
+++ desafio28.js
@@ -1,3 +1,2 @@
 use dataFlights;
-db.voos.deleteMany({ $and: [{ "empresa.nome": "GOL" }, { "passageiros.pagos": { $gt: 5, $lt: 10 } }] });
-// 43 voos deletados
+db.voos.deleteMany({ $and: [{ "empresa.nome": "GOL" }, { "passageiros.pagos": { $gte: 5, $lte: 10 } }] });
