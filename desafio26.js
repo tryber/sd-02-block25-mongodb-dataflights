34a8c43f75a64e486f5d19a47a23e84bdc9b83ea
guicgs <etohlif@protonmail.com>
2020-05-28 19:25:40 -0300

    refat

--- desafio26.js
+++ desafio26.js
@@ -1,2 +1,2 @@
 use dataFlights;
-db.voos.findOne({ $and: [{ litrosCombustivel: { $not: { $gt: 600 } } }, { litrosCombustivel: { $exists: true } }, { "empresa.nome": { $not: { $in: ["GOL", "AZUL"] } } }] });
+db.voos.findOne({ $and: [{ litrosCombustivel: { $not: { $gt: 600 } } }, { litrosCombustivel: { $exists: true } }, { "empresa.nome": { $nin: ["GOL", "AZUL"] } }] });
