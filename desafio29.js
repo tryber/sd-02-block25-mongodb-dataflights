7c3ec7ce4420c6ce4f04b76f09968eeea389469a
guicgs <etohlif@protonmail.com>
2020-05-28 20:15:33 -0300

    encerrado

--- desafio29.js
+++ desafio29.js
@@ -1,3 +1,3 @@
 use dataFlights;
 db.voos.find({ $and: [{ "empresa.nome": "GOL" }, { natureza: "Doméstica" }] }).count();
-db.resumoVoos.insertOne({ "empresa": "GOL", "totalVoosDomesticos": 25828 });
+db.resumoVoos.insertOne({ "empresa": "GOL", "totalVoosDomesticos": 25805 });
