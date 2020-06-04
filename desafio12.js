a447791e775a106c181dfd3e6c9155b80464fd2c
Mateus Talles <mateustalles@gmail.com>
2020-06-04 19:35:14 -0300

    Removida descrição do desafio

--- desafio12.js
+++ desafio12.js
@@ -1,5 +1,3 @@
-Retorne apenas os 10 primeiros documentos com voos da empresa GOL do ano de 2017. Exiba apenas os campos vooId, empresa.nome, aeroportoOrigem.nome, aeroportoDestino.nome, mes e ano.
-
 db.voos.find(
 { "empresa.nome": "GOL", ano: 2017},
 {
@@ -11,4 +9,3 @@ db.voos.find(
     ano: 1,
     _id: 0
     }).limit(10);
-    
