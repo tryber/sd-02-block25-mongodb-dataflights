e0d302005a21badaa9e811d7dea7059903bab817
Andrey <andreytsuzuki@gmail.com>
2020-06-07 18:03:37 -0300

    push

--- desafio29.js
+++ desafio29.js
@@ -1,11 +1,4 @@
-db.voos.count({
-  $and: [
-    { "empresa.nome": { $eq: "GOL" } },
-    { "natureza": { $eq: "Doméstica" } }
-  ]
-});
-
 db.resumoVoos.insert({
-    "empresa": "GOL",
-    "totalVoosDomesticos": 25866
+  "empresa": "GOL",
+  "totalVoosDomesticos": 25866
 });
--- desafio30.js
+++ desafio30.js
@@ -1,12 +1,5 @@
-db.voos.count({
-    $and: [
-        { "empresa.nome": { $eq: "AZUL" } },
-        { "natureza": { $eq: "Doméstica" } }
-    ]
-});
-  
 db.resumoVoos.insert({
-    "empresa": "AZUL",
-    "totalVoosDomesticos": 39622
+  "empresa": "AZUL",
+  "totalVoosDomesticos": 39622
 });
   
